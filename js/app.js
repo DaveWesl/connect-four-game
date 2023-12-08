f07e0068ba2e801f81f0b35bb777384599676f4b
David Wesle <wesledavid1@gmail.com>
2023-12-08 13:50:20 +0100

    added buttons in js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.button-rules').addEventListener('click', openGameRules);
     document.querySelector('.button-check').addEventListener('click', openMainMenu);
+    document.querySelector('.button-pvsp').addEventListener('click', openGameBoard);
+    document.querySelector('.button-menu').addEventListener('click', openIngameMenu);
+    document.querySelector('.button-continue').addEventListener('click', openGameBoardContinue);
+    document.querySelector('.button-quit').addEventListener('click', openMainMenuQuit);
 });
 
 const backgroundRulesElement = document.querySelector('.backgroundRules');
 const backgroundMainMenuElement = document.querySelector('.backgroundMainMenu');
+const backgroundGameBoardStart1Element = document.querySelector('.backgroundGameBoardStart1');
+const backgroundIngameMenuElement = document.querySelector('.backgroundIngameMenu');
 
 function openGameRules() {
-    backgroundRulesElement.style.display = 'flex';
     backgroundMainMenuElement.style.display = 'none';
+    backgroundRulesElement.style.display = 'flex';
 }
 
 function openMainMenu() {
@@ -16,3 +22,22 @@ function openMainMenu() {
     backgroundMainMenuElement.style.display = 'flex';
 }
 
+function openGameBoard() {
+    backgroundMainMenuElement.style.display = 'none';
+    backgroundGameBoardStart1Element.style.display = 'flex';
+}
+
+function openIngameMenu() {
+    backgroundGameBoardStart1Element.style.display = 'none';
+    backgroundIngameMenuElement.style.display = 'flex';
+}
+
+function openGameBoardContinue() {
+    backgroundIngameMenuElement.style.display = 'none';
+    backgroundGameBoardStart1Element.style.display = 'flex';
+}
+
+function openMainMenuQuit() {
+    backgroundIngameMenuElement.style.display = 'none';
+    backgroundMainMenuElement.style.display = 'flex';
+}
